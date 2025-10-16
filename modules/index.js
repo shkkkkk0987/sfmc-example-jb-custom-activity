168c34b7a4b452976b7fe5ae502c3dd9b510ce8a
shkkkkk0987 <qwerty197531@naver.com>
2025-10-16 15:03:55 +0900

    Delete modules/index.js

--- modules/index.js
+++
@@ -1,8 +0,0 @@
-// modules/index.js
-
-module.exports = {
-  discountCode: require('./discount-code'),
-  discountRedemptionSplit: require('./discount-redemption-split'),
-  pushTest: require('./push_test'),
-  // 필요하면 여기에 다른 모듈 추가 가능
-};
